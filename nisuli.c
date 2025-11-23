d796a153e1c6f06b8a5f212938a2ee4f67cecbd3
Nisuli08 <nisulisuwanima08@gmail.com>
2025-11-23 13:16:08 +0530

    2
    2

---
+++ nisuli.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter the number: ");
+	scanf("%d",&num);
+	
+	int x=1,c=0;
+	if(num>0){
+		while(x<=num){
+		if(num%x==0){
+			printf("%d ",x);
+			c++;
+			
+		}
+		x++;
+	}
+	printf("\nFactor Count %d: ",c);
+	if(c<=2){
+		printf("\n\nType:prime number");
+	}
+	else{
+		printf("\n\nType:Composite number");
+	}
+}else{
+	printf("Please enter a positive number");
+}
+getch();
+	}
+	
